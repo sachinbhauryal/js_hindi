8307bf0546d04e6cea9cb3bd095b4b1a0cf62070
sachinbhauryal <itssachin05@gmail.com>
2025-12-18 17:00:42 +0000

    array

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,39 @@
+// Array 
+
+const myarr = [0,1,2,3,4,5]
+const myheros = ["ironMan","superMan","spiderMan"]
+
+// console.log(myarr[0]);
+
+const myarr2 = new Array(8,1,534,24,66)
+// console.log(myarr2[3]);
+
+// ARRAY METHODS 
+
+// myarr.push(6)
+// myarr.push(7)
+// console.log(myarr);
+
+// myarr.pop()
+// myarr.pop()
+// console.log(myarr);
+
+// myarr.unshift(9)
+// console.log(myarr);
+
+// myarr.shift()
+// myarr.shift()
+// myarr.shift()
+// console.log(myarr);
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(4));
+
+const newArray = myarr.join()
+console.log(myarr)
+console.log(typeof(myarr));
+
+console.log(newArray);
+console.log(typeof(newArray));
+
+
