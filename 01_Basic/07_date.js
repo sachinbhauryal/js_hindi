c0e2bcf0c8496ffc7b55a7adc561fe7f90e477ea
sachinbhauryal <itssachin05@gmail.com>
2025-12-18 16:23:43 +0000

    date and timestap

---
+++ 01_Basic/07_date.js
@@ -0,0 +1,40 @@
+// Dates 
+
+let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleDateString());
+
+// console.log(typeof(mydate))
+
+// let myCreatedDate = new Date(2025,0,23)
+// let myCreatedDate = new Date(2025,0,23,5,3)
+let myCreatedDate = new Date("2025-01-24")
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toLocaleString());
+
+// +++++++++++++++++++++++++++Timestamp+++++++++++++++++++++++++
+let myTimeStamp =  Date.now()
+
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate)
+// console.log(newDate.getDate())
+// console.log(newDate.getHours())
+// console.log(newDate.getMinutes())
+// console.log(newDate.getMilliseconds())
+// console.log(newDate.getSeconds())
+// console.log(newDate.getTime())
+// console.log(newDate.getTimezoneOffset())
+
+// console.log(newDate.getMonth()+1)
+// console.log(newDate.getDay()+1)
+
+
+
