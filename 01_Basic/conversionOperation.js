b63e8940ae71dec634512f1414dca7083a13b19e
sachinbhauryal <itssachin05@gmail.com>
2025-12-18 04:14:12 +0000

    conversion of datatypes

---
+++ 01_Basic/conversionOperation.js
@@ -0,0 +1,57 @@
+let score = "3a4h";
+
+// console.log(typeof(score));
+// console.log(typeof score );
+
+let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+
+// ----------------------------------------------------------------
+
+let t = null 
+// console.log(typeof(t));
+
+let tt = Number(null)
+
+// console.log(tt);
+// console.log(typeof(tt));
+
+// -----------------------------------------------------------------
+
+let c = undefined 
+// console.log(typeof(c));
+
+let cc = Number(undefined)
+
+// console.log(cc);
+// console.log(typeof(cc));
+// -------------------------------------------------------------
+// Notes:-
+// "33" => 33
+// "33ggk" => NaN
+// True => 1; False => 0;
+
+//------------------------------------------------------------
+
+let isLoogedIn = "hitesh";
+
+let booleanIsLoggedIn  = Boolean(isLoogedIn)
+// console.log(isLoogedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
+
+// ---------------------------------------------------------------
+// 1=> true ; 0 =>false
+// "" => false
+// "sachin " => true 
+// --------------------------------------------------------
+
+let marks = 45;
+console.log(typeof(marks));
+
+let stringNumber = String(marks);
+console.log(stringNumber)
+console.log(typeof(stringNumber));
+
