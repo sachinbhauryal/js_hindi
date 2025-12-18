7ebd8a9d6a44ece081a81ceff5f59267eac8a774
sachinbhauryal <itssachin05@gmail.com>
2025-12-18 13:36:05 +0000

    num and sum

---
+++ 01_Basic/06_num_maths.js
@@ -0,0 +1,19 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const othernumber = 232.8966
+// console.log(othernumber.toPrecision(4));
+
+const amount = 1000000
+// console.log(amount.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++
+console.log(Math);
+l
