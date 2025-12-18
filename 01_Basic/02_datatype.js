4c352a1ba110d71b09fd7ace7fdfa9182eeaee7d
sachinbhauryal <itssachin05@gmail.com>
2025-12-18 03:34:22 +0000

    datatypes

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,30 @@
+"use strip";  // Treat all javascript code as newer version.
+
+// Alert (3+3) //We are using node.js not browser.
+
+console.log(3+3)
+console.log("Sachin ");
+
+let name = "sachin"
+let age = 20
+let isloggIn = true 
+let state
+
+// numeber =>2 to power 53 
+// bigint 
+// string =>""
+// boolean => true/false
+// null = standalone value
+// undefined =>
+// symbol => unique 
+
+// object
+
+console.log(typeof "sachin"); //string 
+console.log(typeof 343); //number 
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
+
+
+
