eacead7969d8fc945674952df83567402b75ee18
sachinbhauryal <itssachin05@gmail.com>
2025-12-18 15:13:48 +0000

    maths

--- 01_Basic/06_num_maths.js
+++ 01_Basic/06_num_maths.js
@@ -15,5 +15,21 @@ const amount = 1000000
 // console.log(amount.toLocaleString('en-IN'))
 
 // ++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++
-console.log(Math);
-l
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6))
+// console.log(Math.round(4.4))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.min(4,5,2,9,7))
+// console.log(Math.max(8,3,93,23,45))
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
