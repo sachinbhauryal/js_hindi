9ae10973c2f4f7582c8c76a7476d7cea1bbd9b0a
sachinbhauryal <itssachin05@gmail.com>
2025-12-18 03:06:15 +0000

    variables

---
+++ 01_Basic/01_varaiable.js
@@ -0,0 +1,20 @@
+const accountId = 23748
+let accountEmail = "sachin@google.com"
+var accountPassward = "2423"
+accountcity = "bageshwar"
+let accountState 
+
+// accountId = 342    //not allowed
+accountEmail = "sachinbhauryal@ff.com"
+accountPassward="343jh"
+accountcity = "haldwani"
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of the issue in block space and functional scope 
+*/
+console.table([accountId,accountEmail,accountPassward,accountcity,accountState])
+
+
+
