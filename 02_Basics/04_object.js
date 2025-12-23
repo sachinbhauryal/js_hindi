4f7e9d6713a5f5086bbdf263a48dfe6bbb562f3e
sachinbhauryal <itssachin05@gmail.com>
2025-12-23 05:37:42 +0000

    json talk

--- 02_Basics/04_object.js
+++ 02_Basics/04_object.js
@@ -18,15 +18,9 @@ const regularUser ={
 }
 // console.log(regularUser.name.fullName.lastName);
 
-const obj1 = {
-    1:"a",2:"b"
-}
-const obj2 = {
-    3:"a",4:"b"
-}
-const obj3 = {
-    5:"a",6:"b"
-}
+const obj1 = { 1:"a",2:"b"}
+const obj2 = { 3:"a",4:"b"}
+const obj3 = { 5:"a",6:"b"}
 
 // const obj4 = {obj1,obj2,obj3}
 // console.log(obj4);
@@ -34,11 +28,47 @@ const obj3 = {
 // const obj4 = Object.assign({}, obj1,obj2,obj3)
 // console.log(obj4);
 
+
+// here we have use spread .easy and simple to emplement 
 const obj4 = {...obj1,...obj2,...obj3}
 // console.log(obj4);
 
 // console.log(tinderUser);
 // console.log(Object.keys(tinderUser))
 // console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser))
-console.log(tinderUser.hasOwnProperty("id"));
+// console.log(Object.entries(tinderUser))  // make the key value pair in the form of array 
+
+// console.log(tinderUser.hasOwnProperty("id"));
+
+// -----------------------------------------------------
+
+const course = {
+    coursename : "JS in Hindi",
+    price : 999,
+    CourseInstructor : "Sachin"
+}
+// course.CourseInstructor
+// console.log(course.CourseInstructor);
+
+// const{CourseInstructor}=course
+// console.log(CourseInstructor);
+
+//  if the value name is long you can rename it .
+const{CourseInstructor:instructor}=course
+console.log(instructor);
+
+// -----------------------------API----------------------------
+
+// {
+//     "Name":"sachin",
+//     "courseName":"js in hindi",
+//     "Price": "free"
+// }
+
+// API can also be in form of array
+
+[
+    {},
+    {},
+    {}
+]
