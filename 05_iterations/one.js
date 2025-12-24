e988139702cc1e9a1d243f13926277d072282cdb
sachinbhauryal <itssachin05@gmail.com>
2025-12-24 09:49:48 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,19 @@
+// for loop
+n=10
+for (let i = 0; i < n+1; i++) {
+    console.log(i);
+    if (n=5) {
+        console.log("5 is the best number")
+    }
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`outer loop value ${i}`);
+    
+    for(let j = 1; j <= 2;j++){
+// console.log(`inner loop value ${j}`)
+
+
+    }
+    
+}
