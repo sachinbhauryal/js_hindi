c0ed03999ff2eb8bb3d96cd628faad2569be8f41
sachinbhauryal <itssachin05@gmail.com>
2025-12-24 07:16:25 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    default :
+        console.log("default case ");
+        break;
+}
