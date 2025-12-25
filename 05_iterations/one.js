b1c2f14fbb1da4fbe2e0ebda25664659644060d4
sachinbhauryal <itssachin05@gmail.com>
2025-12-25 03:23:07 +0000

    for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,19 +1,43 @@
 // for loop
 n=10
 for (let i = 0; i < n+1; i++) {
-    console.log(i);
+    // console.log(i);
     if (n=5) {
-        console.log("5 is the best number")
+        // console.log("5 is the best number")
     }
 }
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`outer loop value ${i}`);
+for (let i = 1; i <= 10; i++) {
+    // console.log(`outer loop value ${i}`);
     
-    for(let j = 1; j <= 2;j++){
+    for(let j = 1; j <= 10;j++){
 // console.log(`inner loop value ${j}`)
+// console.log(i+' * '+j+' = '+i*j);
+    } 
+}
 
+let hero = ["superMan" , "spiderMan", "batMan", "Ironman"]
 
-    }
+for(let i = 0; i<hero.length;i++){
+    console.log(hero[i]);
     
+}
+
+//break and continue 
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i==5) {
+//         console.log("5 is detected");  
+//         break
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+
+for (let i = 0; i <= 10; i++) {
+    if (i==5) {
+        // console.log("5 is detected");  
+        continue
+    }
+    console.log(`value of i is ${i}`);
 }
