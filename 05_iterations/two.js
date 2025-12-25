adf3ccb242aaf7e8b4452067aee70f200eabffac
sachinbhauryal <itssachin05@gmail.com>
2025-12-25 04:23:17 +0000

    while  do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let index = 1;
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index = index+2
+// }
+
+// let arr= 0
+// let heros = ["spiderman","superman","batman","catman"]
+// while (arr < heros.length) {
+//     console.log(`value is ${heros[arr]}`);
+//  arr++   
+// }
+
+let i=0;
+let heros = ["spiderman","superman","batman","catman"]
+do{
+console.log(`hero is : ${heros[i]}`);
+i++
+}while(i<heros.length)
